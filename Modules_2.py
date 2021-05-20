c8b7e1320651c6dbf5a2ff6d16ab30c8d57c1fff
think_computational <411117005@nitt.edu>
2021-05-20 17:51:08 +0530

    module_3

---
+++ Modules_2.py
@@ -0,0 +1,23 @@
+# import cmath
+# x = 4.10 + 3.0j
+# y = -2.10 - 2.5j
+# # print(x-y)
+# a = complex(2.0, 1.0)
+# # print(a)
+# # print(x+y)
+# # print(x/y)
+# # print("The real part of a is {}".format((x+y).real))
+# # print("The imaginary part is {}".format(a.imag))
+# print(cmath.phase(a))
+# Pol = cmath.polar(a)
+# print(Pol)
+# r, theta = Pol
+# rect = cmath.rect(r, theta)
+# print(rect)
+# exp = cmath.exp(a)
+# print(exp)
+# print(cmath.log(a))
+# print(cmath.sqrt(a))
+# print(cmath.pi)
+
+

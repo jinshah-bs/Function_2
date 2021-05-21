058664e473d2ec42f0ab0e5e0cc0656abddaccfc
think <411117005@nitt.edu>
2021-05-21 17:53:13 +0530

    classess

---
+++ class_1.py
@@ -0,0 +1,34 @@
+# l1 = [10.2, 2.0, 3.5, 18.9, 15.2]
+# l1.sort()
+# print(l1
+# a = 10
+# b = 15
+# print(a+b)
+# print(a.__add__(b))
+
+class CarModel:
+
+    def __init__(self, car, what, capa, fuel_type = "Diesel"):
+        self.model = car
+        self.type = what
+        self.cc = capa
+        self.fuel = fuel_type
+
+    def SetData(self, car, what, capa):
+        self.model = car
+        self.type = what
+        self.cc = capa
+
+    def Printdata(self):
+        print(self.model, self.type, self.cc, self.fuel)
+
+
+Tiago = CarModel("Tiago", "HB", 1000, "Petrol")
+Tiago.Printdata()
+Tigor = CarModel("Tigor", "NB", 1600)
+Tigor.Printdata()
+Tigor.Fuel = "Diesel"
+Tigor.Printdata()
+
+
+
